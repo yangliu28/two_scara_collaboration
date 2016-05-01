3cdea5be1adc6c5e89dc1a61f8b80a453332e46b
yangliu28 <yxl1450@case.edu>
2016-05-01 12:54:06 -0400

    finish debug of cylinder spawner

--- src/cylinder_blocks_spawner.cpp
+++ src/cylinder_blocks_spawner.cpp
@@ -12,7 +12,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <gazebo_msgs/SapwnModel.h>
+#include <gazebo_msgs/SpawnModel.h>
 #include <gazebo_msgs/ApplyBodyWrench.h>
 #include <std_msgs/Int8MultiArray.h>
 
@@ -29,24 +29,24 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh;
 
     // service client for service /gazebo/spawn_urdf_model
-    ros::ServiceClient spawn_model_client;
+    ros::ServiceClient spawn_model_client
         = nh.serviceClient<gazebo_msgs::SpawnModel>("/gazebo/spawn_urdf_model");
     gazebo_msgs::SpawnModel spawn_model_srv_msg;  // service message
     // service client for service /gazebo/apply_body_wrench
-    ros::ServiceClient apply_wrench_client;
+    ros::ServiceClient apply_wrench_client
         = nh.serviceClient<gazebo_msgs::ApplyBodyWrench>("/gazebo/apply_body_wrench");
     gazebo_msgs::ApplyBodyWrench apply_wrench_srv_msg;  // service message
 
     // publisher for /current_cylinder_blocks
     ros::Publisher current_cylinder_publisher
-        = nh.advertise<std_msgs::Int8MultiArray>)("current_cylinder_blocks", 1);
+        = nh.advertise<std_msgs::Int8MultiArray>("current_cylinder_blocks", 1);
     std_msgs::Int8MultiArray current_cylinder_msg;
     current_cylinder_msg.data.clear();
 
     // make sure /gazebo/spawn_urdf_model service is ready
     bool service_ready = false;
     while (!service_ready) {
-        service_ready = ros::service::exists("/gazebo/spawn_urdf_model",true);
+        service_ready = ros::service::exists("/gazebo/spawn_urdf_model", true);
         ROS_INFO("waiting for spawn_urdf_model service");
         ros::Duration(0.5).sleep();
     }
@@ -55,7 +55,7 @@ int main(int argc, char **argv) {
     // make sure /gazebo/apply_body_wrench service is ready
     service_ready = false;
     while (!service_ready) {
-        service_ready = ros::service::exists("/gazebo/apply_body_wrench",true);
+        service_ready = ros::service::exists("/gazebo/apply_body_wrench", true);
         ROS_INFO("waiting for apply_body_wrench service");
         ros::Duration(0.5).sleep();
     }
@@ -71,17 +71,17 @@ int main(int argc, char **argv) {
         return 0;  // return if fail to get parameters
 
     // prepare the xml for service call, read urdf into string
-    std::ifstream inXml;
-    std::stringstream strStream;
+    std::ifstream red_inXml, blue_inXml;
+    std::stringstream red_strStream, blue_strStream;
     std::string red_xmlStr, blue_xmlStr;
     // red cylinder
-    inXml.open(red_cylinder_path.c_str());
-    strStream  << inXml.rdbuf();
-    red_xmlStr = strStream.str();
+    red_inXml.open(red_cylinder_path.c_str());
+    red_strStream << red_inXml.rdbuf();
+    red_xmlStr = red_strStream.str();
     // blue cylinder
-    inXml.open(blue_cylinder_path.c_str());
-    strStream  << inXml.rdbuf();
-    blue_xmlStr = strStream.str();
+    blue_inXml.open(blue_cylinder_path.c_str());
+    blue_strStream << blue_inXml.rdbuf();
+    blue_xmlStr = blue_strStream.str();
 
     // prepare the spawn model service message
     spawn_model_srv_msg.request.initial_pose.position.x = 4.0;
@@ -93,21 +93,25 @@ int main(int argc, char **argv) {
     spawn_model_srv_msg.request.reference_frame = "world";
 
     // prepare the apply body wrench service message
+    ros::Time time_temp(0, 0);
+    ros::Duration duration_temp(0, 1000000);
     apply_wrench_srv_msg.request.wrench.force.x = -0.002;
-    apply_wrench_srv_msg.request.start_time = 0;
-    apply_wrench_srv_msg.request.duration = 1000000;
+    apply_wrench_srv_msg.request.start_time = time_temp;
+    apply_wrench_srv_msg.request.duration = duration_temp;
 
     // begin spawn cylinder blocks and give an initial speed on conveyor
-    int i;  // index the cylinder blocks
+    int i = 0;  // index the cylinder blocks
     while (ros::ok()) {
         std::string index_string = intToString(i);
         std::string model_name;
-        int8 color_index;  // 0 is red, 1 is blue
+        int8_t color_index;  // 0 is red, 1 is blue
 
         // prepare spawn model service message
         spawn_model_srv_msg.request.initial_pose.position.y
-            = (float)rand()/(float)(RAND_MAX) - 0.5;  // random between -0.5 to 0.5
-        if ((rand() - RAND_MAX) > 0) {
+            = (float)rand()/(float)(RAND_MAX) * 0.8 - 0.4;  // random between -0.4 to 0.4
+        ROS_INFO_STREAM("y position of new cylinder: "
+            << spawn_model_srv_msg.request.initial_pose.position.y);
+        if ((rand() - RAND_MAX/2) > 0) {
             // then choose red cylinder
             color_index = 0;
             model_name = "red_cylinder_" + index_string;  // initialize model_name
@@ -119,7 +123,7 @@ int main(int argc, char **argv) {
             // then choose blue cylinder
             color_index = 1;
             model_name = "blue_cylinder_" + index_string;
-            spawn_model_srv_msg.request.model_name = model_name;  // initialize model_name
+            spawn_model_srv_msg.request.model_name = model_name;
             spawn_model_srv_msg.request.robot_namespace = "blue_cylinder_" + index_string;
             spawn_model_srv_msg.request.model_xml = blue_xmlStr;
         }
@@ -128,15 +132,14 @@ int main(int argc, char **argv) {
         if (call_service) {
             if (spawn_model_srv_msg.response.success) {
                 ROS_INFO_STREAM(model_name << " has been spawned");
-                ROS_INFO_STREAM("");  // blank line
             }
             else {
                 ROS_INFO_STREAM(model_name << " spawn failed");
-                ROS_INFO_STREAM("");  // blank line
             }
         }
         else {
-            ROS_ERROR("Fail to connect with gazebo server");
+            ROS_INFO("fail in first call");
+            ROS_ERROR("fail to connect with gazebo server");
             return 0;
         }
 
@@ -148,15 +151,13 @@ int main(int argc, char **argv) {
         if (call_service) {
             if (apply_wrench_srv_msg.response.success) {
                 ROS_INFO_STREAM(model_name << " speed initialized");
-                ROS_INFO_STREAM("");  // blank line
             }
             else {
                 ROS_INFO_STREAM(model_name << " fail to initialize speed");
-                ROS_INFO_STREAM("");  // blank line
             }
         }
         else {
-            ROS_ERROR("Fail to connect with gazebo server");
+            ROS_ERROR("fail to connect with gazebo server");
             return 0;
         }
 
@@ -165,8 +166,12 @@ int main(int argc, char **argv) {
         current_cylinder_msg.data.push_back(color_index);
         current_cylinder_publisher.publish(current_cylinder_msg);
 
+        // loop end, increase index by 1, add blank line
+        i = i + 1;
+        ROS_INFO_STREAM("");
+
         ros::spinOnce();
-        ros::Duration(1.0).sleep();  // spawn a cylinder every 1 second
+        ros::Duration(2.0).sleep();  // frequency control, spawn one cylinder in each loop
 
     }
 

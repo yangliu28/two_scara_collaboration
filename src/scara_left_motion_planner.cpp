7f73046f544d3c83bbcbb7bd03b083b5980e6411
yangliu28 <yxl1450@case.edu>
2016-05-06 15:15:35 -0400

    add all communication for left scara motion planner

--- src/scara_left_motion_planner.cpp
+++ src/scara_left_motion_planner.cpp
@@ -9,8 +9,8 @@
     // publish to topic "/scara_robot_left_rotation2_pos_cmd"
     // subscribe to topic "/cylinder_active_pool"
     // service client to service "/cylinder_pool_claim"
-    // subscribe to topic "/scara_right_upper_boundry"
-    // publish the topic "/scara_left_upper_boundry"
+    // subscribe to topic "/scara_right_upper_boundary"
+    // publish the topic "/scara_left_upper_boundary"
     // action client for gripper control, "gripper_action"
     // service client to get cylinder speed "/gazebo/get_model_state"
 
@@ -21,21 +21,35 @@
     // 4.drop position -> stand by position (high speed)
 // only in motion step 1, the operation may be delayed when the other robot is in operation range
 
+// upper boundary control method:
+    // each robot has a state machine contains a boolean and a number
+    // the boolean indicate if the robot is at upper area of the conveyor
+    // the number indicate the upper boundary the robot goes
+    // each robot check the other robot's state and make change to its own
 
 
 #include <ros/ros.h>
 #include <math.h>
 #include <two_scara_collaboration/cylinder_blocks_poses.h>
 #include <std_msgs/Int8MultiArray.h>
+#include <two_scara_collaboration/scara_upper_boundary.h>
+#include <actionlib/client/simple_action_client.h>
+#include <two_scara_collaboration/scara_gripperAction.h>
+#include <gazebo_msgs/GetModelState.h>
 
 // global variables
 std::vector<double> g_cylinder_x;  // only x y coordinates matter
 std::vector<double> g_cylinder_y;
 std::vector<int> g_cylinder_pool;  // the active pool
+two_scara_collaboration::scara_upper_boundary g_scara_right_upper_boundary;
 
 
-std::vector<double> scara_left_IK(std::vector<double> joints) {
-    // inverse kinematic for the left scara robot
+std::vector<double> scara_left_kinematics(std::vector<double> joints) {
+    // kinematics for the left scara robot
+}
+
+std::vector<double> scara_left_inverse_kinematics(std::vector<double> position) {
+    // inverse kinematics for the left scara robot
     // robot is always positive configured, so only return the position configuration
 }
 
@@ -53,6 +67,12 @@ void cylinderPoolCallback(const std_msgs::Int8MultiArray& cylinder_pool_msg) {
     int pool_size = cylinder_pool_msg.data.size();
     g_cylinder_pool.resize(pool_size);
     g_cylinder_pool = cylinder_pool_msg.data;
+}
+
+void scaraRightUpperBoundaryCallback(const
+    two_scara_collaboration::scara_upper_boundary& upper_boundary_msg) {
+    // update on upper boundary of right scara
+    g_scara_right_upper_boundary = upper_boundary_msg;
 }
 
 
@@ -78,13 +98,40 @@ int main(int argc, char** argv) {
     ros::Subscriber cylinder_pool_subscriber = nh.subscribe("/cylinder_active_pool"
         , 1, cylinderPoolCallback);
     // service client to service "/cylinder_pool_claim"
-    ros::ServiceClient cylinder_pool_claim_client = nh.serviceClient<two_scara_collaboration::
+    ros::ServiceClient cylinder_pool_claim_client
+        = nh.serviceClient<two_scara_collaboration::pool_claim_msg>("cylinder_pool_claim");
+    two_scara_collaboration::pool_claim_msg cylinder_claim_srv_msg;
+    // subscribe to topic "/scara_right_upper_boundary"
+    ros::Subscriber scara_right_upper_boundary_subscriber
+        = nh.subscribe("/scara_right_upper_boundary", 1, scaraRightUpperBoundaryCallback);
+    // publish on topic "/scara_left_upper_boundary"
+    ros::Publisher scara_left_upper_boundary_publisher
+        = nh.advertise<two_scara_collaboration::scara_upper_boundary>("/scara_left_upper_boundary", 1);
+    two_scara_collaboration::scara_left_upper_boundary scara_left_upper_boundary_msg;
+    // action client for gripper control, "gripper_action"
+    actionlib::SimpleActionClient<two_scara_collaboration::scara_gripperAction>
+        scara_gripper_action_client("gripper_action", true);
+    two_scara_collaboration::scara_gripperGoal scara_gripper_goal;
+    // service client to get cylinder speed "/gazebo/get_model_state"
+    ros::Serviceclient get_model_state_client
+        = nh.serviceClient<gazebo_msgs/GetModelState>("/gazebo/get_model_state");
+
+    // check if service "/gazebo/get_model_state" is ready
+    bool service_ready = false;
+    while (!service_ready) {
+        service_ready = ros::service::exists("/gazebo/get_model_state", true);
+        ROS_INFO("waiting for get_model_state service");
+        ros::Duration(0.5).sleep();
+    }
+    ROS_INFO("get_model_state service is ready");
+
+    // get the speed of the last cylinder in queue
+    // so that it has speed instead of falling out of conveyor
+
+
 
 
 }
-
-
-
 
 
 
